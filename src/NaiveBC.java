90fb3f06a914c83330f702a1fd8c10594e64aea7
jcmcallister <jchar.mc@gmail.com>
2015-04-27 23:12:14 -0400

    minor fillout of functions

--- src/NaiveBC.java
+++ src/NaiveBC.java
@@ -1,3 +1,32 @@
-public Class NaiveBC implements DecisionModel{
-	//todo: ALL OF THIS
+public Class NaiveBC implements DecisionModel<String>{
+	
+	public DataSample<String>[] trainingSet;
+	private Outcome<String> decision;
+
+	public NaiveBC(){
+		trainingSet = new DataSample<String>();
+		decision = null;
+	}
+
+	public NaiveBC trainModel(DataSample<String>[] dataset){
+
+	}
+
+	public NaiveBC trainModel(DataSample<String>[] dataset, int maxrows){
+		
+	}
+
+	public Outcome<String> getDecision(NaiveBC model, DataSample<String>[] dataset){
+
+	}
+
+	public Outcome<String> getDecision(){
+		//check if decision != null
+		if(this.decision != null){
+			return this.decision;
+		}else{
+			System.out.println("You need to run getDecision() with a NaiveBC object and a dataset to generate a decision first!");
+		}
+	}
+	
 }
