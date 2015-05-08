67bfbc05be76d6c01f5b06f07011c859aed28be3
Lucas Crawford <lac@uoregon.edu>
2015-05-07 18:26:25 -0700

    updated to allow custom key and username

--- src/interfaces/GooglerExample.java
+++ src/interfaces/GooglerExample.java
@@ -5,7 +5,8 @@ public class GooglerExample {
 	
 	public static void main(String[] args){
 		//Create a new translator for translating to russian
-		Translator translator = new Translator("ru");
+		//	Translator impl = new Translator("ru"); IF YOU DONT HAVE ACCT INFO
+		Translator impl = new Translator("thorrism", "03.315902810:cfeda0c0b0a70c46080a34ffa9e3cea5", "ru");
 		
 		//Alternatively could set language this way:
 		//translator.setLanguage("ru");
@@ -22,7 +23,7 @@ public class GooglerExample {
 		//translator.setLanguage("en");
 		
 		//Let's find the Yandex query results: CHANGE "thorrism" to YOUR ACCOUNT
-		int query = translator.getQuery(translation, "thorrism");
+		int query = translator.getQuery(translation);
 		System.out.println(translation + " has: " + query
 				+ " results on Yandex!");
 	}
